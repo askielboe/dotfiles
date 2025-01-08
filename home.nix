d0c4e289aa1ad2941179d6e6ef99406df9335a93
Andreas Skielboe <skielboe@gmail.com>
2025-01-08 16:30:56 +0100

    move home.file to modules

--- home.nix
+++ home.nix
@@ -8,6 +8,7 @@ in
 {
   imports = [
     ./modules/packages.nix
+    ./modules/file.nix
     ./modules/programs/starship.nix
   ];
 
@@ -27,24 +28,6 @@ in
   # release notes.
   home.stateVersion = "24.11"; # Please read the comment before changing.
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    ".config/ghostty/config".source = dotfiles/ghostty/config;
-    ".config/nvim/lua".source = dotfiles/nvim/lua;
-    "rustic.toml".source = dotfiles/rustic/rustic.toml;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
 
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
