d431c4077fa1460d123cb1890aa0d6de0d0dc8a4
Andreas Skielboe <skielboe@gmail.com>
2025-01-08 16:30:56 +0100

    move home.file to modules

---
+++ modules/file.nix
@@ -0,0 +1,8 @@
+{ ... }:
+{
+  home.file = {
+    ".config/ghostty/config".source = ../dotfiles/ghostty/config;
+    ".config/nvim/lua".source = ../dotfiles/nvim/lua;
+    "rustic.toml".source = ../dotfiles/rustic/rustic.toml;
+  };
+}
