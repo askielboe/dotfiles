4226f981b95dc4f1c41ae36448a3c67f933bde23
Andreas Skielboe <skielboe@gmail.com>
2025-01-08 16:22:57 +0100

    move starship config to module

--- home.nix
+++ home.nix
@@ -6,6 +6,10 @@ let
   email = "skielboe@gmail.com";
 in
 {
+  imports = [
+    ./modules/programs/starship.nix
+  ];
+
   nixpkgs.config.allowUnfree = true;
 
   # Home Manager needs a bit of information about you and the paths it should
@@ -193,36 +197,6 @@ in
           exec $SHELL
         }
       '';
-    };
-
-    starship = {
-      enable = true;
-      enableZshIntegration = true;
-      format = lib.concatStrings [
-        "[](surface0)"
-        "$os"
-        "$username"
-        "[](bg:peach fg:surface0)"
-        "$directory"
-        "[](fg:peach bg:green)"
-        "$git_branch"
-        "$git_status"
-        "[](fg:green bg:teal)"
-        "$c"
-        "$rust"
-        "$golang"
-        "$nodejs"
-        "$php"
-        "$java"
-        "$kotlin"
-        "$haskell"
-        "$python"
-        "[](fg:blue bg:purple)"
-        "$time"
-        "[ ](fg:purple)"
-        "$line_break"
-        "$character"
-      ];
     };
 
     fzf = {
