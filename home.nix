efc61923075df501ba98737377dcc8dc88bb9ee9
Andreas Skielboe <skielboe@gmail.com>
2025-01-08 15:53:29 +0100

    home-manager init

--- home.nix
+++ home.nix
@@ -6,6 +6,8 @@ let
   email = "skielboe@gmail.com";
 in
 {
+  nixpkgs.config.allowUnfree = true;
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "askielboe";
@@ -18,11 +20,7 @@ in
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
-
-  nixpkgs.config = {
-    allowUnfree = true;
-  };
+  home.stateVersion = "24.11"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -352,5 +350,6 @@ in
     };
   };
 
+  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
