6894d92fecdb497877f9a8669911bc1e304b15ed
Andreas Skielboe <skielboe@gmail.com>
2025-01-08 21:42:29 +0100

    add rclone.age

--- modules/age.nix
+++ modules/age.nix
@@ -1,6 +1,9 @@
-
-{ ... }:
+{ config, ... }:
 
 {
   age.secrets.anthropic.file = ../secrets/anthropic.age;
+  age.secrets.rclone = {
+    file = ../secrets/rclone.age;
+    path = "${config.home.homeDirectory}/.config/rclone/rclone.conf";
+  };
 }
