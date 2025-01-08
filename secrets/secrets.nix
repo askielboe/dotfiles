1c7af87b0b47dd68a731d509e208806278c5852e
Andreas Skielboe <skielboe@gmail.com>
2025-01-08 17:39:18 +0100

    add secrets

---
+++ secrets/secrets.nix
@@ -0,0 +1,10 @@
+# This secrets.nix file is not imported into your NixOS configuration. It's
+# only used for the agenix CLI tool (example below) to know which public keys
+# to use for encryption.
+# https://github.com/ryantm/agenix?tab=readme-ov-file#tutorial
+let
+  user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAaKU21SMHSEW8TiKQNmNROJOiV3yH4CZDisS+muPrde";
+in
+{
+  "anthropic.age".publicKeys = [ user ];
+}
