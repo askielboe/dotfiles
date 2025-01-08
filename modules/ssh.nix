f9b17b3d368fda37caf4199b39ac19eaa791ef7d
Andreas Skielboe <skielboe@gmail.com>
2025-01-08 16:43:04 +0100

    move rest of config to modules

---
+++ modules/ssh.nix
@@ -0,0 +1,49 @@
+{ ... }:
+
+let
+  user = "askielboe";
+in
+{
+  programs.ssh = {
+    enable = true;
+    # includes = [ "/Users/${user}/.ssh/config_external" ];
+    matchBlocks = {
+      "*" = {
+        extraOptions = {
+          IdentityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
+        };
+      };
+      "github.com" = {
+        hostname = "github.com";
+        user = "git";
+        identitiesOnly = true;
+        identityFile = "/Users/${user}/.ssh/id_ed25519-github.pub";
+      };
+      "flextribe" = {
+        hostname = "github.com";
+        user = "git";
+        identitiesOnly = true;
+        identityFile = "/Users/${user}/.ssh/id_ed25519_flextribe.pub";
+      };
+      "storagebox-restic" = {
+        hostname = "your-storagebox.de";
+        user = "u407515-sub6";
+        identitiesOnly = true;
+        identityFile = "/Users/${user}/.ssh/id_ed25519-storagebox.pub";
+      };
+      "paperless" = {
+        hostname = "135.181.80.183";
+        port = 54056;
+        user = "paperless";
+      };
+      "dobby" = {
+        hostname = "192.168.1.12";
+      };
+      "garage-hetzner" = {
+        hostname = "49.13.75.42";
+        identitiesOnly = true;
+        identityFile = "/Users/${user}/.ssh/id_ed25519-hetzner-garage.pub";
+      };
+    };
+  };
+}
