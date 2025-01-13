9d5b3caa682cd5f80263b6ef9afbba3fbd411b01
Andreas Skielboe <skielboe@gmail.com>
2025-01-13 11:03:30 +0100

    nvim oil show hidden

--- dotfiles/nvim/lua/plugins/oil.lua
+++ dotfiles/nvim/lua/plugins/oil.lua
@@ -1,7 +1,9 @@
 return {
   "stevearc/oil.nvim",
   config = function()
-    require("oil").setup({})
+    require("oil").setup({
+      show_hidden = true,
+    })
   end,
   keys = {
     { "<leader>o", ":Oil<CR>", { desc = "Open oil" } },
