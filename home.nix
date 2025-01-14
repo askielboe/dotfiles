aab7455bd378431d8ffc48a4b2bb58a24003a028
Andreas Skielboe <skielboe@gmail.com>
2025-01-14 09:04:52 +0100

    home.nix formatting

--- home.nix
+++ home.nix
@@ -11,40 +11,35 @@
     ./modules/ssh.nix
   ];
 
-  nixpkgs.config.allowUnfree = true;
-
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "askielboe";
-  home.homeDirectory = "/Users/askielboe";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
-
-  home.sessionVariables = {
-    VISUAL = "nvim";
-    EDITOR = "nvim";
-    XDG_CONFIG_HOME = "$HOME/.config";
-    DIRENV_LOG_FORMAT = "";
-    OP_ACCOUNT = "***REMOVED-SECRET***";
-    ANTHROPIC_API_KEY = "$(cat ${config.age.secrets.anthropic.path})";
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = (_: true);
+    };
   };
 
-  home.shellAliases = {
-    o = "open .";
-    lg = "lazygit";
-    he = "cd ~/.config/home-manager/ && nvim && cd -";
+  home = {
+    username = "askielboe";
+    homeDirectory = "/Users/askielboe";
+    stateVersion = "24.11"; # Keep this unchanged
+
+    sessionVariables = {
+      VISUAL = "nvim";
+      EDITOR = "nvim";
+      XDG_CONFIG_HOME = "$HOME/.config";
+      DIRENV_LOG_FORMAT = "";
+      OP_ACCOUNT = "***REMOVED-SECRET***";
+      ANTHROPIC_API_KEY = "$(cat ${config.age.secrets.anthropic.path})";
+    };
+
+    shellAliases = {
+      o = "open .";
+      lg = "lazygit";
+      he = "cd ~/.config/home-manager/ && nvim && cd -";
+    };
   };
 
-  catppuccin = {
-    enable = true;
-  };
+  catppuccin.enable = true;
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
