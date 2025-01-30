ec1541a7b36fab24d2616e024df6a7dc697f88c6
Andreas Skielboe <skielboe@gmail.com>
2025-01-30 17:05:42 +0100

    pueue config

--- modules/file.nix
+++ modules/file.nix
@@ -5,6 +5,7 @@
     ".config/ghostty/config".source = ../dotfiles/ghostty/config;
     ".config/nvim/stylua.toml".source = ../dotfiles/nvim/stylua.toml;
     ".config/nvim/lua".source = ../dotfiles/nvim/lua;
+    ".config/pueue/pueue.yml".source = ../dotfiles/pueue/pueue.yml;
     "rustic.toml".source = ../dotfiles/rustic/rustic.toml;
     ".prettierrc".source = ../dotfiles/prettierrc;
   };
