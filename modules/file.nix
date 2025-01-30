68eb048bc29583f22a0aa2e83e533e0f0c0b34a2
Andreas Skielboe <skielboe@gmail.com>
2025-01-30 17:17:17 +0100

    sort file.nix

--- modules/file.nix
+++ modules/file.nix
@@ -3,10 +3,10 @@
 {
   home.file = {
     ".config/ghostty/config".source = ../dotfiles/ghostty/config;
-    ".config/nvim/stylua.toml".source = ../dotfiles/nvim/stylua.toml;
     ".config/nvim/lua".source = ../dotfiles/nvim/lua;
+    ".config/nvim/stylua.toml".source = ../dotfiles/nvim/stylua.toml;
     ".config/pueue/pueue.yml".source = ../dotfiles/pueue/pueue.yml;
-    "rustic.toml".source = ../dotfiles/rustic/rustic.toml;
     ".prettierrc".source = ../dotfiles/prettierrc;
+    "rustic.toml".source = ../dotfiles/rustic/rustic.toml;
   };
 }
