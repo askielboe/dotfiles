4f7bdd966131b4b87e2dae66be2ff276165ffe19
Andreas Skielboe <skielboe@gmail.com>
2025-03-11 10:34:45 +0100

    darwin: dock apps

--- modules/darwin/settings/system.nix
+++ modules/darwin/settings/system.nix
@@ -18,6 +18,22 @@
 
         # Style options
         show-recents = false;
+
+        persistent-apps = [
+          # { app = "/System/Library/CoreServices/Finder.app"; }
+          { app = "/System/Applications/Calendar.app"; }
+          { app = "/Applications/Brave Browser.app"; }
+          { app = "/Applications/Todoist.app"; }
+          { app = "/System/Applications/Messages.app"; }
+          { app = "/Applications/Signal.app"; }
+          { app = "/Applications/Slack.app"; }
+          { app = "/Applications/Ghostty.app"; }
+          { app = "/Applications/1Password.app"; }
+          { app = "/Applications/Bear.app"; }
+          { app = "/Applications/Spotify.app"; }
+          { app = "/Applications/DEVONthink 3.app"; }
+          { app = "/Applications/Zed.app"; }
+        ];
       };
 
       finder = {
