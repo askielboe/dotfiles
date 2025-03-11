2fda394857b51e97325012d113699e300ee7a830
Andreas Skielboe <skielboe@gmail.com>
2025-03-11 09:09:48 +0100

    darwin: migrate homebrew

--- modules/darwin/settings/homebrew.nix
+++ modules/darwin/settings/homebrew.nix
@@ -1,11 +1,42 @@
 { ... }: {
   homebrew = {
     enable = true;
+    onActivation.cleanup = "zap";
     caskArgs.no_quarantine = true;
 
     casks = [
+      "1password"
+      "1password-cli"
+      "appcleaner"
+      "arq"
+      "bitwarden"
+      "brave-browser"
+      "cyberduck"
+      "devonthink"
+      "discord"
+      "docker"
+      "domzilla-caffeine"
+      "ghostty"
+      "gitbutler"
       "jordanbaird-ice" # Hide menu bar icons
+      "little-snitch"
+      "macfuse"
+      "notion"
+      "pocket-casts"
+      "postico"
+      "proxyman"
+      "raycast"
+      "reactotron"
+      "signal"
+      "slack"
+      "sonos"
       "spotify"
+      "stats"
+      "steam"
+      "tor-browser"
+      "transmission"
+      "vlc"
+      "zed"
     ];
   };
 }
