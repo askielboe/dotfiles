10e26cf0cefb59b3f15ed56cd5fa8e30486b4698
Andreas Skielboe <skielboe@gmail.com>
2025-03-28 11:09:31 +0100

    nvim: git blame and gitsigns

---
+++ modules/home-manager/nixvim/plugins/git/default.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+{
+  programs.nixvim.plugins.gitsigns = {
+    enable = true;
+  };
+  programs.nixvim.plugins.gitblame = {
+    enable = true;
+    settings = {
+      date_format = "%r"; # relative date
+    };
+  };
+  programs.nixvim.keymaps = [
+    {
+      mode = "n";
+      key = "<leader>go";
+      action = "<CMD>GitBlameOpenCommitURL<CR>";
+      options = {
+        desc = "Open commit URL";
+      };
+    }
+  ];
+}
