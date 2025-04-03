68a78b29940dbd2cbff4f68c0c7ad356a4e44361
Andreas Skielboe <skielboe@gmail.com>
2025-04-03 17:20:19 +0200

    nvim: mini

---
+++ modules/home-manager/nixvim/plugins/mini/surround.nix
@@ -0,0 +1,20 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  programs.nixvim.plugins.mini.modules.surround = {
+    mappings = {
+      add = "gsa";
+      delete = "gsd";
+      find = "gsf";
+      find_left = "gsF";
+      highlight = "gsh";
+      replace = "gsr";
+      update_n_lines = "gsn";
+    };
+  };
+}
