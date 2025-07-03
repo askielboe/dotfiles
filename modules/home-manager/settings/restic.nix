0498dbbf9f2b16a35c2c33309bc7ced4dc68c8f1
Andreas Skielboe <skielboe@gmail.com>
2025-07-03 23:07:08 +0200

    feat: add sops configuration for resticprofile.yaml secret

--- modules/home-manager/settings/restic.nix
+++ modules/home-manager/settings/restic.nix
@@ -2,5 +2,6 @@
 {
   sops.secrets."resticprofile.yaml" = {
     path = "/Users/askielboe/.config/restic/profile.conf";
+    mode = "0600";
   };
 }
--- modules/sops/default.nix
+++ modules/sops/default.nix
@@ -2,5 +2,10 @@
   sops = {
     age.keyFile = "/Users/askielboe/.config/nix/modules/sops/age/keys.txt";
     defaultSopsFile = ./.sops.yaml;
+    secrets = {
+      "resticprofile.yaml" = {
+        sopsFile = ./secrets/resticprofile.yaml;
+      };
+    };
   };
 }
