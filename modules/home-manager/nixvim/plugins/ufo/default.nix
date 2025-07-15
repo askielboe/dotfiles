b21982ba8a245f13dc3789ac5476963e716be5e4
Andreas Skielboe <skielboe@gmail.com>
2025-07-15 14:04:18 +0200

    nvim: ufo

---
+++ modules/home-manager/nixvim/plugins/ufo/default.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+# Code folding
+{
+  programs.nixvim = {
+    plugins = {
+      nvim-ufo = {
+        enable = true;
+        settings = {
+          providerSelector = ''
+            function(bufnr, filetype, buftype)
+              return { 'lsp', 'indent' }
+            end
+          '';
+        };
+      };
+    };
+    opts = {
+      foldlevel = 99;
+      foldlevelstart = 99;
+      foldenable = true;
+    };
+  };
+}
