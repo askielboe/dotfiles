73d8d28ca139b592adbae81f54a46f037528fa5c
askielboe <skielboe@gmail.com>
2025-07-25 17:37:24 +0000

    impure build for non-free unstable claude code

--- build-and-switch-linux.sh
+++ build-and-switch-linux.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Building home-manager configuration..."
-nix --extra-experimental-features "nix-command flakes" build '.#homeConfigurations.askielboe.activationPackage'
+env -i NIXPKGS_ALLOW_UNFREE=1 PATH="$PATH" nix --extra-experimental-features "nix-command flakes" build '.#homeConfigurations.askielboe.activationPackage' --impure
 
 echo "Switching to new home-manager configuration..."
 ./result/activate
