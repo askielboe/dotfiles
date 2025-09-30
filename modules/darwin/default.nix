da5cbda016da651cdb140b15632478efc33b210f
Andreas Skielboe <skielboe@gmail.com>
2025-09-30 13:06:29 +0200

    nix: remove cachix

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -22,8 +22,6 @@
     optimise.automatic = true;
     settings = {
       experimental-features = "nix-command flakes";
-      extra-substituters = [ "https://devenv.cachix.org" ];
-      extra-trusted-public-keys = [ "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=" ];
     };
   };
 
