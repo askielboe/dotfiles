39f6b3f5f46f03ac9daf00b4ea08a39b964f9898
Andreas Skielboe <skielboe@gmail.com>
2026-01-02 11:15:03 +0100

    python: lsp

--- modules/home-manager/settings/python.nix
+++ modules/home-manager/settings/python.nix
@@ -4,9 +4,11 @@
     (python312.withPackages (
       ps: with ps; [
         ipython
+        jedi-language-server
         jupyter
         matplotlib
         numpy
+        python-lsp-server
         requests
         scipy
         torch
