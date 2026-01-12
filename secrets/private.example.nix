c9ee47a1c84f1bacaecfc25b0575de0ae0ed92ec
Andreas Skielboe <skielboe@gmail.com>
2026-01-12 13:52:47 +0100

    nix: private.nix example

---
+++ secrets/private.example.nix
@@ -0,0 +1,72 @@
+{
+  user = {
+    name = "Your Name";
+    email = "your.email@example.com";
+    username = "yourusername";
+    homeDirectory = "/Users/yourusername";
+    signingKey = "ssh-ed25519 AAAA...your-public-key";
+  };
+
+  accounts = {
+    opAccount = "YOUR-1PASSWORD-ACCOUNT-ID";
+    awsProfiles = {
+      default = {
+        region = "us-east-1";
+        opItem = "your-1password-item-id";
+      };
+      secondary = {
+        name = "profile-name";
+        region = "eu-central-1";
+        opItem = "your-1password-item-id";
+      };
+    };
+  };
+
+  machine = {
+    computerName = "your-hostname";
+  };
+
+  apiKeys = {
+    bear = "your-bear-api-token";
+  };
+
+  ssh = {
+    storagebox = {
+      hostname = "your-storagebox.de";
+      user = "your-storagebox-user";
+    };
+    paperless = {
+      hostname = "0.0.0.0";
+      port = 22;
+      user = "paperless";
+    };
+    synology = {
+      hostname = "192.168.1.10";
+      user = "yourusername";
+    };
+    dobby = {
+      hostname = "192.168.1.12";
+      user = "yourusername";
+    };
+    macmini = {
+      hostname = "192.168.1.11";
+      user = "yourusername";
+    };
+    garageHetzner = {
+      hostname = "0.0.0.0";
+    };
+    nix = {
+      hostname = "0.0.0.0";
+      user = "root";
+    };
+    simplyTm = {
+      hostname = "your-host.example.com";
+      user = "your-domain.com";
+    };
+    k3s = {
+      hostname = "0.0.0.0";
+      user = "root";
+      identityFile = "~/.ssh/id_rsa-your-key";
+    };
+  };
+}
