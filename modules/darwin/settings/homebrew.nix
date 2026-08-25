51aa091f8f89caf8db190f004f85ca51a5c5011a
Andreas Skielboe <skielboe@gmail.com>
2026-08-25 13:52:47 +0200

    Use App Store Bitwarden for Safari

--- modules/darwin/settings/homebrew.nix
+++ modules/darwin/settings/homebrew.nix
@@ -77,7 +77,6 @@ in
       "aerial"
       "appcleaner"
       "arq"
-      "bitwarden"
       "brave-browser"
       "claude"
       "claude-status-bar"
@@ -117,6 +116,7 @@ in
     masApps = {
       "1Password for Safari" = 1569813296;
       Bear = 1091189122;
+      Bitwarden = 1352778147;
       DaisyDisk = 411643860;
       Keynote = 361285480;
       LookAway = 6747192301;
