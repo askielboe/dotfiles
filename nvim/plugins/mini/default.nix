4e78013b255298e1499ce250e5621e9a175a3291
Andreas Skielboe <skielboe@gmail.com>
2026-09-01 15:14:32 +0200

    nvim: fold Mini settings into module

--- nvim/plugins/mini/default.nix
+++ nvim/plugins/mini/default.nix
@@ -1,14 +1,31 @@
 {
-  imports = [
-    ./ai.nix
-    ./starter.nix
-    ./surround.nix
-  ];
-
   plugins.mini = {
     enable = true;
     mockDevIcons = true;
     modules = {
+      ai = {
+        n_lines = 500;
+        custom_textobjects = {
+          __raw = ''
+            {
+              o = require("mini.ai").gen_spec.treesitter({
+                a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+                i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+              }),
+              f = require("mini.ai").gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
+              c = require("mini.ai").gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }),
+              t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
+              d = { "%f[%d]%d+" },
+              e = {
+                { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
+                "^().*()$",
+              },
+              u = require("mini.ai").gen_spec.function_call(),
+              U = require("mini.ai").gen_spec.function_call({ name_pattern = "[%w_]" }),
+            }
+          '';
+        };
+      };
       icons = { };
       indentscope = {
         symbol = "│";
@@ -17,6 +34,52 @@
       move = { };
       pairs = { };
       pick = { };
+      starter = {
+        content_hooks = {
+          "__unkeyed-1.adding_bullet" = {
+            __raw = "require('mini.starter').gen_hook.adding_bullet()";
+          };
+          "__unkeyed-2.indexing" = {
+            __raw = "require('mini.starter').gen_hook.indexing('all', { 'Builtin actions' })";
+          };
+          "__unkeyed-3.padding" = {
+            __raw = "require('mini.starter').gen_hook.aligning('center', 'center')";
+          };
+        };
+        evaluate_single = true;
+        header = ''
+          ███╗   ██╗██╗██╗  ██╗██╗   ██╗██╗███╗   ███╗
+          ████╗  ██║██║╚██╗██╔╝██║   ██║██║████╗ ████║
+          ██╔██╗ ██║██║ ╚███╔╝ ██║   ██║██║██╔████╔██║
+          ██║╚██╗██║██║ ██╔██╗ ╚██╗ ██╔╝██║██║╚██╔╝██║
+          ██║ ╚████║██║██╔╝ ██╗ ╚████╔╝ ██║██║ ╚═╝ ██║
+        '';
+        items = {
+          "__unkeyed-1.buildtin_actions" = {
+            __raw = "require('mini.starter').sections.builtin_actions()";
+          };
+          "__unkeyed-2.recent_files_current_directory" = {
+            __raw = "require('mini.starter').sections.recent_files(10, false)";
+          };
+          "__unkeyed-3.recent_files" = {
+            __raw = "require('mini.starter').sections.recent_files(10, true)";
+          };
+          "__unkeyed-4.sessions" = {
+            __raw = "require('mini.starter').sections.sessions(5, true)";
+          };
+        };
+      };
+      surround = {
+        mappings = {
+          add = "gsa";
+          delete = "gsd";
+          find = "gsf";
+          find_left = "gsF";
+          highlight = "gsh";
+          replace = "gsr";
+          update_n_lines = "gsn";
+        };
+      };
       trailspace = { };
       visits = { };
     };
